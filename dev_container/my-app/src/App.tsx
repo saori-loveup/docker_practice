cb31ff459aa81f16285ddf117cbb201b4d08d9f9
Saori Ito <saori@itoo.info>
2024-09-04 14:28:56 +0900

    アプリケーションの編集

--- dev_container/my-app/src/App.tsx
+++ dev_container/my-app/src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onAddButtonClicked = () => {
+    console.log('Add Clicked');
+    setCount( (prevCount) => prevCount + 1);
+  };
+  const onDecButtonClicked = () => {
+    console.log('Dec Clicked');
+    setCount( (prevCount) => {
+      return prevCount -1;
+    } );
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div>
+          <p>current : {count} </p>
+          <button onClick={onAddButtonClicked}>Add</button>
+          <button onClick={onDecButtonClicked}>Dec</button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
